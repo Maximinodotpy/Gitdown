b11101641a0d9bb75514135890dd57962e0b2cb2
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-02-03 08:38:11 +0100

    changes

---
+++ includes/scripts/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('MIRROR_PATH', 'mirror/');
+define('GTW_ROOT_PATH', __DIR__.'/');
+
+define('PLUGIN_PREFIX', 'gtw');
+
+/* Admin Menu Slugs */
+define('GTW_SETTINGS_SLUG', PLUGIN_PREFIX.'-settings');
+define('GTW_ARTICLES_SLUG', PLUGIN_PREFIX.'-article-manager');
+
+?>
--- index.php
+++ index.php
@@ -12,6 +12,69 @@ Text Domain:  git-to-wordpress
 Domain Path:  /languages
 */
 
-include 'config.php';
+/* http://localhost/git-to-wordpress/wordpress/wp-admin/admin.php */
 
-include 'scripts/pages.php';
+
+
+class GIT_TO_WORDPRESS {
+
+    function __construct() {
+        include 'includes/scripts/config.php';
+        include 'includes/scripts/pages.php';
+
+        // Activation Hook
+        register_activation_hook(
+            __FILE__,
+            function() {}
+        );
+        
+        add_action('admin_init', function () {
+            $settingsSectionSlug = PLUGIN_PREFIX.'_settings_section';
+            $page = 'reading';
+
+            register_setting($page, 'test_setting');
+
+            add_settings_section(
+                $settingsSectionSlug,
+                'Git To WordPress Settings',
+                function () {
+                    ?>fasdf<?php
+                },
+                $page
+            );
+        
+            // register a new field in the "wporg_settings_section" section, inside the "reading" page
+            add_settings_field(
+                'glob_pattern',
+                'Glob Pattern',
+                function () {
+                    ?>
+                    <input type="text" name="test_setting" value="<?=get_option('test_setting')?>">
+                    <p class="description">blalblalblal</p>
+                    <?php
+                },
+                $page,
+                $settingsSectionSlug
+            );
+        });
+        
+        /* register_uninstall_hook(
+            __FILE__,
+            function() {
+        
+            }
+        ); */
+    }
+
+    
+};
+
+$gtw = new GIT_TO_WORDPRESS();
+
+// Deactivation Hook
+/* register_deactivation_hook(
+    __FILE__,
+    function() {
+
+    }
+); */
