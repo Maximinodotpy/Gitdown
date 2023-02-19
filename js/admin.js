1916645185c5fdb59cff43ad7f65e52545ae2479
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-02-19 16:57:21 +0100

    added eslint and some style changes

--- js/admin.js
+++ js/admin.js
@@ -1,35 +1,44 @@
+// eslint-disable-next-line no-undef
 const { createApp } = Vue
 
 const vueApp = createApp({
     data() {
         return {
             articles: [],
+            search_query: '',
             metadata: {
                 repo_url: 'https://github.com/Maximinodotpy/articles.git'
             }
         }
     },
+    
     async mounted() {
         this.sync()
     },
-    methods: {
-        async callAJAX(desiredData) {
-            const data = new FormData();
-            data.append('action', desiredData.action);
-            data.append('slug', desiredData.slug);
 
+    methods: {
+
+        async callAJAX(desiredData) {
+            const form_data = new FormData()
+
+            for (const key in desiredData) {
+                form_data.append(key, desiredData[key])
+            }
+
+            // eslint-disable-next-line no-undef
             const re = await fetch(ajaxurl, {
                 method: 'POST',
-                body: data,
+                body: form_data,
             })
-            return await re.json();
+            return await re.json()
         },
+
         async updateArticle(slug) {
             const loaderElement = this.$refs[slug][0]
 
-            console.log('Updating: '+slug);
+            console.log('Updating: '+slug)
 
-            loaderElement.style.visibility = 'visible';
+            loaderElement.style.visibility = 'visible'
 
             const newData = await this.callAJAX({
                 action: 'update_article',
@@ -38,19 +47,19 @@ const vueApp = createApp({
 
             this.articles.find(article => {
                 if (article.remote.slug == slug) {
-                    article.local = newData;
-                    article._is_published = true;
+                    article.local = newData
+                    article._is_published = true
                 }
-            });
+            })
 
-            loaderElement.style.visibility = 'hidden';
+            loaderElement.style.visibility = 'hidden'
         },
+
         async deleteArticle(slug) {
             const loaderElement = this.$refs[slug][0]
-            loaderElement.style.visibility = 'visible';
+            loaderElement.style.visibility = 'visible'
 
-
-            console.log('Deleting: '+slug);
+            console.log('Deleting: '+slug)
 
             const newData = await this.callAJAX({
                 action: 'delete_article',
@@ -60,26 +69,42 @@ const vueApp = createApp({
             if (newData) {
                 this.articles.forEach(article => {
                     if (article.remote.slug == slug) {
-                        article._is_published = false;
-                        article.local = {};
+                        article._is_published = false
+                        article.local = {}
                     }
                 })
             }
 
-            loaderElement.style.visibility = 'hidden';
+            loaderElement.style.visibility = 'hidden'
+        },
+
+        updateAllArticles() {
+            console.log('Updating All')
+
+            this.articles.forEach(article => {
+                this.updateArticle(article.remote.slug)
+            })
+        },
+
+        deleteAll() {
+            console.log('Deleting all articles')
+
+            this.articles.forEach(article => {
+                this.deleteArticle(article.remote.slug)
+            })
         },
 
         async sync() {
-            this.articles = [];
+            this.articles = []
             
             this.articles = (await this.callAJAX({
                 action: 'get_all_articles',
             })).reverse()
-            console.log(this.articles);
+            console.log(this.articles)
         }
     }
 })
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     vueApp.mount('#vue_app')
 })
--- views/articles.php
+++ views/articles.php
@@ -1,22 +1,33 @@
 <div class="wrap gitdown_ui" id="vue_app">
     <h1>Manage Git Articles ...</h1>
-    <p>...  for <code>{{ metadata.repo_url }}</code>.</p>
+    <p>... for <code>{{ metadata.repo_url }}</code>.</p>
 
     <p>According to the glob pattern <code><?php echo get_option(GD_SETTING_GLOB) ?></code> and your set resolver function the following files could be found.</p>
 
-    <!-- <p>Keep in mind that all articles are identified by their <code>slug</code>/<code>post_name</code>. Thats why it is shown here in the Github column. If you change the slug in the markdown file, Gitdown wont recognize that the articles belong together.</p> -->
+    <div style="display: flow-root;">
+        <ul class='subsubsub'>
+            <span style="color: hsl(0, 0%, 60%)">Published </span>
+            {{ articles.filter(article => article._is_published).length }}
+            <span style="color: hsl(0, 0%, 60%)"> out of </span>
+            {{ articles.length }}
+        </ul>
+    </div>
 
-    <!-- <p>Contact me if <a href="mailto:info@maximmaeder.com?subject=Gitdown: ">here</a> if you found some issues or have any questions.</p> -->
-    
-    
-    <div class="tablenav top">
-        <a href="<?php echo esc_url($_SERVER['REQUEST_URI'] . '&gd_action=publish_all') ?>" class="button button-primary">Update / Publish All</a>
-        
-        <a href="<?php echo esc_url($_SERVER['REQUEST_URI'] . '&gd_action=delete_all') ?>" class="button">Delete All</a>
-        
+    <br>
+
+    <div>
+        <button @click="updateAllArticles()" class="button button-primary">Update All</button>
+
+        <button @click="deleteAll()" class="button">Delete All</button>
+
         <button @click="sync()" class="button">Reload</button>
-        
+
         <a href="<?php echo esc_url(dirname(plugin_dir_url(__FILE__), 1) . '/files/example.zip') ?>" download="example" class="button">Download Example Folder Structure</a>
+
+        <p class="search-box">
+            <label class="screen-reader-text" for="post-search-input">Search Posts:</label>
+            <input type="search" id="post-search-input" v-model="search_query" placeholder="Search" />
+        </p>
     </div>
 
     <br>
@@ -31,46 +42,50 @@
         </thead>
         <tbody>
             <tr v-for="item in articles">
-                <td>
-                    <p class="row-title" title="Post Name">{{ item.remote.name }}</p>
-                    <p title="Post Slug">{{ item.remote.slug }}</p>
-                    <p title="description">{{ item.remote.description }}</p>
-                    <p title="Category">{{ item.remote.category }}</p>
-                </td>
-                <td>
-                    <template v-if="item._is_published">
-                        <div class="row-title">✅ Is on Wordpress</div>
+                <template v-if="item.remote.name.toLowerCase().includes(search_query.toLowerCase())">
+                    <td>
+                        <p class="row-title" title="Post Name">{{ item.remote.name }}</p>
+                        <p title="Post Slug">{{ item.remote.slug }}</p>
+                        <p title="description">{{ item.remote.description }}</p>
+                        <p title="Category">{{ item.remote.category }}</p>
+                    </td>
+                    <td>
+                        <template v-if="item._is_published">
+                            <div class="row-title">✅ Is on Wordpress</div>
 
-                        <br/>
+                            <br />
 
-                        <div>ID: <code>{{ item.local.ID }}</code></div>
-                        <div>Slug: <code>{{ item.local.post_name }}</code></div>
-                        <div>Excerpt: <code>{{ item.local.post_excerpt }}</code></div>
-                        <div>Status: <code>{{ item.local.post_status }}</code></div>
+                            <div>ID: <code>{{ item.local.ID }}</code></div>
+                            <div>Slug: <code>{{ item.local.post_name }}</code></div>
+                            <div>Excerpt: <code>{{ item.local.post_excerpt }}</code></div>
+                            <div>Status: <code>{{ item.local.post_status }}</code></div>
+                            <br>
+                            <div><a target="_blank" :href="item.local.guid">Open in new Tab</a></div>
+                            <br>
+
+                            <?php if (has_post_thumbnail($postData[GD_LOCAL_KEY]['ID'])) : ?>
+                                <img src="" alt="Thumbnail not Found" style="max-width: 130px; filter: grayscale(50%); opacity: 0.5">
+                            <?php endif; ?>
+                        </template>
+                    </td>
+                    <td>
+                        <div v-if="item._is_published">
+                            <button class="button action button-primary" @click="updateArticle(item.remote.slug)">Update</button>
+                            <button class="button action" @click="deleteArticle(item.remote.slug)">Delete</button>
+                        </div>
+
+                        <div v-else>
+                            <button class="button action" @click="updateArticle(item.remote.slug)">Publish</button>
+                        </div>
+
                         <br>
-                        <div><a target="_blank" :href="item.local.guid">Open in new Tab</a></div>
-                        <br>
 
-                        <?php if (has_post_thumbnail($postData[GD_LOCAL_KEY]['ID'])) : ?>
-                            <img src="" alt="Thumbnail not Found" style="max-width: 130px; filter: grayscale(50%); opacity: 0.5">
-                        <?php endif; ?>
-                    </template>
-                </td>
-                <td>
-                    <div v-if="item._is_published">
-                        <button class="button action button-primary" @click="updateArticle(item.remote.slug)">Update</button>
-                        <button class="button action" @click="deleteArticle(item.remote.slug)">Delete</button>
-                    </div>
-                        
-                    <div v-else>
-                        <button class="button action" @click="updateArticle(item.remote.slug)">Publish</button>
-                    </div>
-
-                    <div :ref="item.remote.slug" style="visibility: hidden; display: flex; align-items: center; font-weight: 600">
-                        <img src="<?php echo GD_ROOT_URL.'images/loader.svg' ?>" alt="Loader" style="width: 30px">
-                        Loading
-                    </div>
-                </td>
+                        <div :ref="item.remote.slug" style="visibility: hidden; display: flex; align-items: center; font-weight: 600; font-size: 1rem;">
+                            <img src="<?php echo GD_ROOT_URL . 'images/loader.svg' ?>" alt="Loader" style="width: 30px">
+                            <span>Loading</span>
+                        </div>
+                    </td>
+                </template>
             </tr>
         </tbody>
     </table>
