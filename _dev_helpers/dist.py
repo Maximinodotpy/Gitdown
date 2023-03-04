0e81f24aa9bb221ce486eec158c7c2208c2fbab8
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-03-04 10:41:09 +0100

    changes made to dits file

--- _dev_helpers/dist.py
+++ _dev_helpers/dist.py
@@ -1,7 +1,43 @@
+# Imports
 import shutil
 import os
+from pathlib import Path
+import svn.remote as svn_remote
+import json
+import argparse
 
-def clearFolder(folder):
+# Constants
+SVN_PATH = Path(os.getcwd()) / Path('_svn')
+SVN_URL = 'http://plugins.svn.wordpress.org/gitdown'
+CONFIG = json.load(open('_dev_helpers/config.json'))
+
+
+# Setup Argument Parser
+parser = argparse.ArgumentParser(
+    prog = 'Gitdown Distribution',
+)
+parser.add_argument('version_mode', choices=['major', 'minor', 'patch'])
+
+ARGS = parser.parse_args()
+print(ARGS.version_mode)
+
+
+# Functions
+def create_folder(new_path):
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+
+def path_exits(path):
+    return os.path.exists(path)
+
+def copy_folder(source, destination, ignore=[]):
+    print('\nCopying %s to %s' % (source, destination))
+    shutil.copytree(source, destination, ignore = shutil.ignore_patterns(*ignore))
+
+def clear_folder(folder):
+    print('\nClearing %s' % folder)
+    if not path_exits(folder): return
+
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
         try:
@@ -12,29 +48,46 @@ def clearFolder(folder):
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+    os.rmdir(folder)
+
+def get_folders_within_folder(folder):
+    filenames= os.listdir (folder) # get all files' and folders' names in the current directory
+
+    result = []
+    for filename in filenames:
+        if (os.path.isdir(Path(folder) / Path(filename))):
+            result.append(filename)
+
+    result.sort()
+    result.reverse()
+    return result
+
+# Final Script
+
+create_folder(SVN_PATH)
+
+while (not path_exits(SVN_PATH / Path('.svn'))):
+    input(f'Checkout Repository to "{SVN_PATH}" and then press enter ...')
 
 
-print('Creating Dist Zip Folder ...')
+# Copy Assets Folder
+asset_folder = SVN_PATH / Path('assets')
+clear_folder(asset_folder)
+copy_folder('assets', asset_folder, ignore = [ 'banner.ai', 'icon.ai', 'overview.md', 'gitdown-video.mp4' ])
 
-
-distFolderName = 'dist'
-cwd = os.getcwd()
-distFolderPATH = os.path.join(cwd, distFolderName)
-
-print('Clearing ...')
-clearFolder(distFolderPATH)
-
+trunk_folder = SVN_PATH / Path('trunk')
 ignore = [
     # Folders
     '.git',
     'dist',
+    'assets',
     'languages',
     'test-repo',
     'logs',
     '_dev_helpers',
     'node_modules',
     '_releasing',
-
+    '_svn',
     # Files
     '.eslintrc.json',
     'eslintrc.json',
@@ -47,21 +100,28 @@ ignore = [
     '.gitignore',
     'gitdown-video.mp4',
     'input.css',
-
     '*.ai',
 ]
+clear_folder(trunk_folder)
+copy_folder('.', trunk_folder, ignore = ignore)
 
-print('Copying ...')
-shutil.copytree(cwd, os.path.join(distFolderPATH, 'temp_gitdown'), ignore = shutil.ignore_patterns(*ignore))
+tags_folder = SVN_PATH / Path('tags')
+latest_version = get_folders_within_folder(tags_folder)[0]
+
+latest_major = int(latest_version.split('.')[0])
+latest_minor = int(latest_version.split('.')[1])
+latest_patch = int(latest_version.split('.')[2])
+
+new_version = ''
+
+if (ARGS.version_mode == 'patch'):
+    new_version = f'{latest_major}.{latest_minor}.{latest_patch + 1}'
+elif (ARGS.version_mode == 'minor'):
+    new_version = f'{latest_major}.{latest_minor + 1}.0'
+elif (ARGS.version_mode == 'major'):
+    new_version = f'{latest_major + 1}.0.0'
+
+copy_folder(trunk_folder, tags_folder / Path(new_version))
 
 
-print('Zipping ...')
-shutil.make_archive(os.path.join(distFolderPATH, 'gitdown'), "zip", os.path.join(distFolderPATH, 'temp_gitdown'))
-
-print('Clearing ...')
-""" clearFolder(os.path.join(distFolderPATH, 'temp_gitdown')) """
-try:
-    os.remove(os.path.join(distFolderPATH, 'temp_gitdown'))
-except:
-    pass
-print('Finished')
+print('\nDont forget to now bump of the version number in your readme.txt and to commit the changes via a SVN Client.')
