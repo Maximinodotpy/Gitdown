bd5cce79423dc5a2d8f043524498ad41c525f427
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-03-24 11:10:34 +0100

    Added Typescript to project.

--- js/admin.ts
+++ js/admin.ts
@@ -1,8 +1,11 @@
+
+
 // @ts-ignore
 if (window.Vue) {
     // eslint-disable-next-line no-undef
     // @ts-ignore
-    const { createApp } = Vue;
+    const { createApp } = Vue
+    
     const vueApp = createApp({
         data() {
             return {
@@ -16,109 +19,128 @@ if (window.Vue) {
                     coerced_slugs: 0,
                 },
                 online: true
-            };
+            }
         },
+    
         mounted() {
-            this.online = navigator.onLine;
+            this.online = navigator.onLine
             console.log(this.online);
             window.addEventListener('online', (event) => {
                 console.log("You are now connected to the network.");
                 this.online = true;
-                this.sync();
+                this.sync()
             });
             window.addEventListener('offline', (event) => {
                 console.log("You are no longer connected to the network.");
                 this.online = false;
             });
-            this.sync();
+
+            this.sync()
         },
+    
         methods: {
             async callAJAX(desiredData) {
-                const form_data = new FormData();
+                const form_data = new FormData()
+    
                 for (const key in desiredData) {
-                    form_data.append(key, desiredData[key]);
+                    form_data.append(key, desiredData[key])
                 }
+    
                 // eslint-disable-next-line no-undef
                 // @ts-ignore
                 const re = await fetch(ajaxurl, {
                     method: 'POST',
                     body: form_data,
-                });
-                return await re.json();
+                })
+                return await re.json()
             },
+    
             async update_post(slug) {
-                const loaderElement = this.$refs[slug][0];
-                loaderElement.style.visibility = 'visible';
-                console.log(`%cUpdating Post: %c${slug}`, 'font-weight: bold', 'font-weight: unset');
+                const loaderElement = this.$refs[slug][0]
+                loaderElement.style.visibility = 'visible'
+    
+                console.log(`%cUpdating Post: %c${slug}`, 'font-weight: bold', 'font-weight: unset')
+
                 this.callAJAX({
                     action: 'update_article',
                     slug: slug,
                 }).then(newData => {
                     this.articles.find(article => {
                         if (article.remote.slug == slug) {
-                            article.local = newData.new_post;
-                            article._is_published = true;
-                            article.last_updated = newData.last_updated;
+                            article.local = newData.new_post
+                            article._is_published = true
+                            article.last_updated = newData.last_updated
                         }
-                    });
+                    })
                 }).catch(error => {
-                    console.log(error);
-                    if (this.online)
-                        this.sync();
+                    console.log(error)
+                    if (this.online) this.sync()
                 }).finally(() => {
-                    loaderElement.style.visibility = 'hidden';
-                });
+                    loaderElement.style.visibility = 'hidden'
+                })
             },
+    
             async delete_post(slug) {
-                const loaderElement = this.$refs[slug][0];
-                loaderElement.style.visibility = 'visible';
-                console.log(`%cDeleting: %c${slug}`, 'font-weight: bold', 'font-weight: unset');
+                const loaderElement = this.$refs[slug][0]
+                loaderElement.style.visibility = 'visible'
+    
+                console.log(`%cDeleting: %c${slug}`, 'font-weight: bold', 'font-weight: unset')
+    
                 this.callAJAX({
                     action: 'delete_article',
                     slug: slug,
                 }).then(() => {
                     this.articles.forEach(article => {
                         if (article.remote.slug == slug) {
-                            article._is_published = false;
-                            article.local = {};
+                            article._is_published = false
+                            article.local = {}
                         }
-                    });
+                    })
                 }).catch(error => {
-                    console.log(error);
-                    if (this.online)
-                        this.sync();
+                    console.log(error)
+                    if (this.online) this.sync()
                 }).finally(() => {
-                    loaderElement.style.visibility = 'hidden';
-                });
+                    loaderElement.style.visibility = 'hidden'
+                })
             },
+    
             updateAllArticles() {
-                console.log('Updating All ...');
+                console.log('Updating All ...')
+    
                 this.articles.forEach(article => {
-                    this.update_post(article.remote.slug);
-                });
+                    this.update_post(article.remote.slug)
+                })
             },
+    
             deleteAll() {
-                console.log('Deleting All articles ...');
+                console.log('Deleting All articles ...')
+    
                 this.articles.forEach(article => {
-                    this.delete_post(article.remote.slug);
-                });
+                    this.delete_post(article.remote.slug)
+                })
             },
-            async sync() {
+    
+            async sync() {            
                 this.callAJAX({
                     action: 'get_all_articles',
                 }).then(response => {
-                    this.articles = response.posts.reverse();
-                    this.reports = response.reports;
+                    this.articles = response.posts.reverse()
+                    this.reports = response.reports
                 }).catch(error => {
-                    console.log(error);
-                });
+                    console.log(error)
+                })
             },
         }
-    });
+    })
+    
+    
+    
     document.addEventListener('DOMContentLoaded', () => {
-        const appNode = document.querySelector('#vue_app');
+        const appNode = document.querySelector('#vue_app')
+
         if (appNode) {
-            vueApp.mount(appNode);
+            vueApp.mount(appNode)
         }
-    });
+    })
+
 }
